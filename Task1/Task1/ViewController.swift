679796bc25e96f0c2a5b57edeb7edf0894ca8a12
kmizuno1 <kmizuno@appdevteam.work>
2019-11-18 15:03:32 +0900

    リストを作成、ソートボタンを追加

--- Task1/Task1/ViewController.swift
+++ Task1/Task1/ViewController.swift
@@ -8,13 +8,56 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
 
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    var NAMEList:[Name] = [Name]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        NAMEList.append(Name(data: (1,"Shiro")))
+        NAMEList.append(Name(data: (2,"Lina")))
+        NAMEList.append(Name(data: (3,"Wakaba")))
+        NAMEList.append(Name(data: (4,"Rin")))
     }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return NAMEList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //セルを取得する
+        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        //セルに表示する値を固定する
+        cell.textLabel!.text = NAMEList[indexPath.row].name
+        return cell
+    }
+    
+    @IBAction func sortByName(_ sender: Any) {
+        NAMEList.sort{$0.name < $1.name}
+        tableView.reloadData()
+    }
+    
+    @IBAction func sortById(_ sender: Any) {
+        NAMEList.sort{$0.id < $1.id}
+        tableView.reloadData()
+    }
+    
 
 
+}
+
+class Name:NSObject {
+    var id:Int
+    var name:String
+    init(data:(Int,String)) {
+        id = data.0
+        name = data.1
+    }
 }
 
